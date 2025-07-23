241d6d1a6f2cd32f52698727128c51e4415e0ed4
Richard Franks <rfranks@commure.com>
2025-07-23 04:44:15 -0400

    fix(games):  Remove broken codex pirates game for now.

--- src/games/pirates/constants.ts
+++
@@ -1,6 +0,0 @@
-export const MAX_AMMO = 6;
-export const SKY_COLOR = "#79a8d1";
-export const DEFAULT_CURSOR =
-  "url('/assets/shooting-gallery/PNG/HUD/crosshair_red_small.png') 16 16, auto";
-export const SHOT_CURSOR =
-  "url('/assets/shooting-gallery/PNG/Objects/shot_brown_large.png') 16 16, auto";
